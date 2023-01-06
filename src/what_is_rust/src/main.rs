f0c89e2c713f9f595448e0bec6f75284ca91159c
박영석 <yeongseok.park@mirero.co.kr>
2023-01-06 19:59:21 +0900

    러스트가 추구하는 것

---
+++ src/what_is_rust/src/main.rs
@@ -0,0 +1,23 @@
+use std::rc::Rc;
+use std::sync::{Arc, Mutex};
+
+fn main() {
+    let a = 10;
+    let b = Box::new(20);
+    let c = Rc::new(Box::new(30));
+    let d = Arc::new(Mutex::new(40));
+    println!("a:{:?}, b : {:?}, c:{:?}, d:{:?}", a,b,c,d)
+
+
+    
+    //test_equals();
+}
+
+
+fn test_equals(){
+    let a = 10;
+    // 비교(==)를 써야하는 곳에 대입(=)을 하면 컴파일 에러가 난다.
+    if a == 10 {
+        println!("a equals ten");
+    }
+}
